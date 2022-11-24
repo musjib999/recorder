93c11c3262f96654fe23f07ab26b8c0002d78db3
Musa Jibril <musjib999@gmail.com>
2022-11-25 00:34:52 +0100

    sound recording and sound play done

--- lib/presentation/screens/record.dart
+++ lib/presentation/screens/record.dart
@@ -20,31 +20,55 @@ class _RecorderState extends State<Recorder> {
       appBar: AppBar(
         title: const Text('Recorder'),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(15.0),
-        child: BlocBuilder<RecordBloc, RecordState>(
-          builder: (context, state) {
-            if(state.status.isRecording){
-              return RecordingButtons(recordService: recordService);
-            }else if(state.status.isPaused){
-              return PauseRecordingButtons(recordService: recordService);
-            }else if(state.status.isResumed){
-              return RecordingButtons(recordService: recordService);
-            }else if(state.status.isStopped){
-              return BlocListener<RecordBloc, RecordState>(
-                listener: (context, state){
-                  if(state.status == RecorderStatus.stop){
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Record Saved')));
-                  }
-                },
-                child: InitialRecordingButtons(recordService: recordService),
-              );
-            }
-            return InitialRecordingButtons(recordService: recordService);
-          },
+      body: BlocListener<RecordBloc, RecordState>(
+        listener: (context, state) {
+          if (state.status.isStopped) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Record Saved'),
+              ),
+            );
+            Navigator.of(context).push(
+              Player.route(soundPath: state.path!),
+            );
+          } else if (state.status.isPaused) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Paused'),
+              ),
+            );
+          } else if (state.status.isResumed) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Resumed'),
+              ),
+            );
+          } else if (state.status.isRecording) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Recording'),
+              ),
+            );
+          }
+        },
+        child: Container(
+          margin: const EdgeInsets.all(15.0),
+          child: BlocBuilder<RecordBloc, RecordState>(
+            builder: (context, state) {
+              if (state.status.isRecording) {
+                return RecordingButtons(recordService: recordService);
+              } else if (state.status.isPaused) {
+                return PauseRecordingButtons(recordService: recordService);
+              } else if (state.status.isResumed) {
+                return RecordingButtons(recordService: recordService);
+              } else if (state.status.isStopped) {
+                return InitialRecordingButtons(recordService: recordService);
+              }
+              return InitialRecordingButtons(recordService: recordService);
+            },
+          ),
         ),
       ),
     );
   }
 }
-
